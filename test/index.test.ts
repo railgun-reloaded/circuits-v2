754372aea42f09c388ec6f4106b09a53ed6efc6c
jacobmakarsky <jacobmakarsky@gmail.com>
2025-04-04 17:39:11 +0700

    even better documentation, test assert fix

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import { hook, test } from 'brittle'
 import { snarkJsCircuitInputs, snarkJsProofs, snarkJsPublicInputs, standardProofs, standardPublicInputs, testVectors } from './test-vectors'
-import { cleanupSnarkJS, prove, verify } from '../src/index'
+import { cleanupSnarkJS, Proof, prove, verify } from '../src/index'
 import { extractPublicInputsFromCircuitInputs, snarkJSToStandardProof, standardToSnarkJSInput, standardToSnarkJSProof, standardToSnarkJSPublicInputs } from '../src/formatter'
 
 test('Should prove', async function (assert) {
@@ -36,7 +36,7 @@ test('Should ensure formatting is correct for SnarkjsProof', async function (ass
 test('Should ensure formatting is correct for Proof', async function (assert) {
   // For each circuit
   standardProofs.forEach((standardProof, i) => {
-  // Format a standard proof to snarkJs format
+    // Format a standard proof to snarkJs format
     const returnedSnarkJsProof = standardToSnarkJSProof(standardProof)
 
     // Ensure the formatted proof is correct
@@ -69,13 +69,10 @@ test('Should ensure formatting is correct for extracted PublicInputs', async fun
   // For each circuit
   testVectors.forEach((testVector, i) => {
     // Ensure testVector proof exists
-    if (!testVector.proof) {
-      console.log('No proof found for test vector', i)
-      return
-    }
+    assert.ok(testVector.proof, `Test vector ${i} is missing a proof, which is required for this test`)
 
     // Extract PublicInputs from existing CircuitInputs
-    const returnedPublicInputs = extractPublicInputsFromCircuitInputs(testVector.inputs, testVector.proof)
+    const returnedPublicInputs = extractPublicInputsFromCircuitInputs(testVector.inputs, testVector.proof as Proof)
 
     // Ensure the formatted public inputs are correct
     assert.alike(returnedPublicInputs, standardPublicInputs[i])
